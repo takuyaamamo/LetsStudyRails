6bb63bd7a5d7cc69cdaf9cd120b44122e70dc05d
ama <takuyaamano@amamo.local>
2019-04-07 08:51:44 +0900

    [finish]3-4

--- db/migrate/20190406233617_create_posts.rb
+++ db/migrate/20190406233617_create_posts.rb
@@ -1,7 +1,10 @@
 class CreatePosts < ActiveRecord::Migration[5.2]
   def change
     create_table :posts do |t|
-
+      # create_tableという処理の do ~ end 内に書き込むことによってカラムを定義できます。
+      # t.データ型名 :カラム名
+      t.string :title
+      t.string :body
       t.timestamps
     end
   end
