9a6596269a401c53c75dbd2543382b050318b560
ama <takuyaamano@amamo.local>
2019-04-07 22:15:47 +0900

    [fix][finish]3-8

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '9ed50a025f0307fd4a8e0b8b97c06a3084e2ae22ca60fdfad22e554d17b68de02fd1e8ce3d9333d2272cfa4df06a4d9fe5a0fe800829ff889bf5d7465018b3e4'
