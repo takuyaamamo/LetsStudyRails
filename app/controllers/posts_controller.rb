e67babbfd669331b0d4f669f58d29fb510de5a1e
ama <takuyaamano@amamo.local>
2019-04-07 09:46:08 +0900

    [finish]3-5

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,22 @@
+class PostsController < ApplicationController
+  def new
+    # 空のモデルをビューへ渡す
+    @post = Post.new
+  end
+
+  def create
+    # ストロングパラメータを使用
+    post = Post.new(post_params)
+    # DBへ保存
+    post.save
+    # トップ画面へリダイレクト
+    redirect_to '/top'
+  end
+  # メソッドの前にprivateと書くことにより、アクションとしては認識されない（メソッドの名前はモデル名_paramsとすることが多いです）
+  private
+
+  def post_params
+
+    params.require(:post).permit(:title, :body)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'posts/new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'top' => 'root#top'
+  # .../postsというURLにpostでリクエストした時にpostsコントローラーのcreateアクションが呼び出される
+  post 'posts' => 'posts#create'
 end
