faf38fb3518db0a1e4ab3082b01da5a515ac17cb
Ama <Ama@amacbook.local>
2019-04-07 20:52:14 +0900

    [start][finish]3-7

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -20,6 +20,17 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    post = Post.find(params[:id])
+    post.update(post_params)
+    redirect_to post_path(post.id)
+  end
+
   # メソッドの前にprivateと書くことにより、アクションとしては認識されない（メソッドの名前はモデル名_paramsとすることが多いです）
   private
 
--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,8 @@ Rails.application.routes.draw do
   get 'posts' => 'posts#index'
   # .../posts/1 や .../posts/3 に該当する
   get 'posts/:id' => 'posts#show', as: 'post'
+
+  get 'posts/:id/edit' => 'posts#edit', as: 'edit_post'
+
+  patch 'posts/:id' => 'posts#update', as: 'update_post'
 end
